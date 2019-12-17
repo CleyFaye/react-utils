fcf092c6df4022b3edeb82b909fb19bde579a8f2
Gabriel Paul 'Cley Faye' Risterucci <gabriel@keeex.net>
2019-12-17 13:36:07 +0100

    Fixed inEnum() validator

--- src/validator/enum.js
+++ src/validator/enum.js
@@ -1,6 +1,6 @@
 export const inEnum = (values, message) => value => {
   if (!values.includes(value)) {
     return message 
-      || `Must be one of ${values.join(",").map(val => `"${val}"`)}`;
+      || `Must be one of ${values.map(val => `"${val}"`).join(",")}`;
   }
 };
